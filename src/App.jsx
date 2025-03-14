1f3892a5ada37d9fc31690848d4a9ddd52786fa6
fatih-suslu <fatihsuslu98@gmail.com>
2025-03-14 23:53:29 +0300

    React-router-dom added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
 import "./reset.css";
 import "./App.css";
+import { Link, Route, Switch } from "react-router-dom/cjs/react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="mainpage-container">
-      <div className="left-column"></div>
-      <div className="center-column">
-        <h1 className="title">Selamlar</h1>
-      </div>
-      <div className="right-column"></div>
-    </div>
+    <main>
+      <Switch>
+        <Route path="/" exact>
+          <div className="mainpage-container">
+            <div className="left-column"></div>
+            <div className="center-column">
+              <h1 className="title">Selamlar</h1>
+            </div>
+            <div className="right-column"></div>
+          </div>
+        </Route>
+        <Route path="/details">
+          <div className="details-container">
+            <div className="details">
+              <h2>Dizi detay</h2>
+              <Link to="/">Anasayfaya Dön</Link>
+            </div>
+          </div>
+        </Route>
+      </Switch>
+    </main>
   );
 }
 
